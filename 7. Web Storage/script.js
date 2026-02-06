5988a1bfdf7b167b24842deefc2273118fdb1c31
Sallesg82 <davisallesg@yahoo.com>
2026-02-06 11:08:27 -0300

    Teste de commit no zed

--- 7. Web Storage/script.js
+++ 7. Web Storage/script.js
@@ -1,9 +1,2 @@
 var nome = "";
-
-if (typeof localStorage == "underfined") {
-  localStorage.nome = prompt("Digite seu nome");
-}
-
-nome = localStorage.nome;
-
-document.getElementById("nome").innerHTML = nome;
+teste;
