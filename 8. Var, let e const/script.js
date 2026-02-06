d02c392c33711a49f88b193e492a3ec8505a1d5c
Sallesg82 <davisallesg@yahoo.com>
2026-02-06 14:15:36 -0300

    Update da lição 8, aprendi a diferenças entre Var, Let e Const.

--- 8. Var, let e const/script.js
+++ 8. Var, let e const/script.js
@@ -16,4 +16,38 @@ if (nome == "Matheus") {
 
 console.log(sobrenome); // vai dar erro.
 
-//-------------------------------------------------------------------
+// =============================================================================
+
+// Var dentro de uma lista
+var lista = [1, 2, 3];
+
+for (var i in lista) {
+  console.log(lista[i]);
+}
+i;
+
+// For dentro de uma lista
+for (let b in lista) {
+  console.log(lista[b]);
+}
+b; // não funciona fora do escopo do bloco, então dará erro esse B
+
+// =============================================================================
+
+let nome = "Matheus";
+var idade = 10;
+
+if (idade == 10) {
+  // O LET funciona normalmente dentro da condição IF,
+  //mesmo criado fora.
+  console.log(nome);
+}
+
+// =============================================================================
+
+// Const
+
+const nome = "Lucas";
+lucas;
+
+nome = "Lucas Silva"; // Não funciona, pois é uma constante e não se muda.
